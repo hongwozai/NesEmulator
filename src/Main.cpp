e7397826871f9c3e2c825d9cdc1d5edbc7aa0df9
zeya <zeyalu@tencent.com>
2019-05-22 19:57:59 +0800

    框架初步搭建

---
+++ src/Main.cpp
@@ -0,0 +1,20 @@
+/*************************************************
+ ** Copyright(c) 2019, luzeya
+ ** All rights reserved
+ **
+ ** 文件名：Main.cpp
+ ** 创建人：zeya
+ ** 创建日期： 2019-05-22
+ **
+ ** 描  述：简要描述文件内容
+ ** 注  意：本程序是否有些特殊说明
+ **************************************************/
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    cout << "hello world!" << endl;
+    return 0;
+}
